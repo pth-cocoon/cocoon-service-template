d5519c5148ac4ea0a179097d37ce9b5343b42a8a
cocoon <6631097@qq.com>
2020-06-22 16:23:06 +0800

    完善补充部分细节，注释，工具类

--- common-core/src/main/java/icu/cocoon/core/resp/IPageInfo.java
+++ common-core/src/main/java/icu/cocoon/core/resp/IPageInfo.java
@@ -1,14 +1,20 @@
 package icu.cocoon.core.resp;
 
+import io.swagger.annotations.ApiModelProperty;
+
 /**
  * @author Cocoon
  * @date 2020/4/9  12:05
+ * @node 分页信息
  */
 public interface IPageInfo<T> extends IResp<T> {
 
+  @ApiModelProperty("当前页码")
   long getCurrent();
 
+  @ApiModelProperty("每页数量")
   long getSize();
 
+  @ApiModelProperty("总数量")
   long getTotal();
 }
